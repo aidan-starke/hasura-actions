ece24cf164f774eda45c81e495f1e914493b828d
aidan-starke <starke.aidan@gmail.com>
2022-08-25 08:30:39 +1200

    Add `GET_FULL_BLOCK` endpoint

--- server.ts
+++ server.ts
@@ -2,7 +2,11 @@ import bodyParser from "body-parser";
 import { execute } from "@/libs/utils";
 import express, { type Express } from "express";
 import { ApiPromise, WsProvider } from "@polkadot/api";
-import { API_ENDPOINT, GET_BLOCK_WITH_VALIDATOR } from "@/libs/constants";
+import {
+	API_ENDPOINT,
+	GET_BLOCK_WITH_VALIDATOR,
+	GET_FULL_BLOCK,
+} from "@/libs/constants";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -20,11 +24,34 @@ const main = async (server: Express) => {
 			const { data, errors } = await execute(GET_BLOCK_WITH_VALIDATOR, {
 				blockHash,
 			});
-
 			if (errors) return res.status(400).json(errors[0]);
 
 			return res.json({
 				...data.app_blocks[0],
+				validator: header.author.toString(),
+			});
+		} catch (error) {
+			return res.status(400).json({
+				message: error.message,
+			});
+		}
+	});
+
+	server.post("/GetFullBlock", bodyParser.json(), async (req, res) => {
+		try {
+			const { id } = req.body.input;
+
+			const { data, errors } = await execute(GET_FULL_BLOCK, {
+				id,
+			});
+			if (errors) return res.status(400).json(errors[0]);
+
+			const header = await api.derive.chain.getHeader(
+				data.app_blocks_by_pk.hash
+			);
+
+			return res.json({
+				...data.app_blocks_by_pk,
 				validator: header.author.toString(),
 			});
 		} catch (error) {
